fdcfe6f2a24579314da292d587ab527306c582f1
Kara Alexandra <ardnaxelarak@gmail.com>
2021-09-27 14:12:56 -0700

    Include VARIA rando anti-softlock patches

--- build.sh
+++ build.sh
@@ -6,7 +6,20 @@ echo Building Super Metroid + Zelda 3 Randomizer
 find . -name build.py -exec python3 {} \;
 
 cd resources
-python3 create_dummies.py 00.sfc ff.sfc
+python3 create_dummies.py z_00.sfc z_ff.sfc 1
+python3 create_dummies.py sm_00.sfc sm_ff.sfc 3
+
+for f in ../src/sm/randopatches/ips/*.ips; do
+  echo Applying $f
+  flips --apply $f sm_00.sfc sm_00_applied.sfc
+  mv sm_00_applied.sfc sm_00.sfc
+  flips --apply $f sm_ff.sfc sm_ff_applied.sfc
+  mv sm_ff_applied.sfc sm_ff.sfc
+done
+
+python3 create_exhirom.py sm_00.sfc z_00.sfc 00.sfc 0x00
+python3 create_exhirom.py sm_ff.sfc z_ff.sfc ff.sfc 0xff
+
 ./asar --no-title-check --symbols=wla --symbols-path=../build/zsm.sym ../src/main.asm 00.sfc
 ./asar --no-title-check --symbols=wla --symbols-path=../build/zsm.sym ../src/main.asm ff.sfc
 python3 create_ips.py 00.sfc ff.sfc zsm.ips
--- resources/create_dummies.py
+++ resources/create_dummies.py
@@ -3,17 +3,18 @@ import sys
 import os
 
 if sys.argv[1] == "":
-	print("create_dummies.py <00_file> <ff_file>")
+	print("create_dummies.py <00_file> <ff_file> <size_in_mb>")
 	sys.exit()
 else:
 	zero_name = sys.argv[1]
 	ff_name = sys.argv[2]
+	size = int(sys.argv[3] or "6")
 
 fo_z = open(os.path.dirname(os.path.realpath(__file__)) + "/" + zero_name, "wb")
 fo_f = open(os.path.dirname(os.path.realpath(__file__)) + "/" + ff_name, "wb")
 
-fo_z.write(bytes([0x00] * 1024 * 1024 * 6))
-fo_f.write(bytes([0xff] * 1024 * 1024 * 6))
+fo_z.write(bytes([0x00] * 1024 * 1024 * size))
+fo_f.write(bytes([0xff] * 1024 * 1024 * size))
 
 fo_z.close()
 fo_f.close()
